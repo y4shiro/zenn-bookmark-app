e9eb8e8fd2d111167f2c5395d7e9901b990012ed
y4shiro <y4shironao@gmail.com>
2022-07-30 10:55:22 +0900

    update: prettier の printWidth を 120 に変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,10 +19,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className='mt-3 text-2xl'>
-          Get started by editing{' '}
-          <code className='rounded-md bg-gray-100 p-3 font-mono text-lg'>
-            pages/index.tsx
-          </code>
+          Get started by editing <code className='rounded-md bg-gray-100 p-3 font-mono text-lg'>pages/index.tsx</code>
         </p>
 
         <div className='mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full'>
@@ -31,9 +28,7 @@ const Home: NextPage = () => {
             className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
           >
             <h3 className='text-2xl font-bold'>Documentation &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Find in-depth information about Next.js features and its API.
-            </p>
+            <p className='mt-4 text-xl'>Find in-depth information about Next.js features and its API.</p>
           </a>
 
           <a
@@ -41,9 +36,7 @@ const Home: NextPage = () => {
             className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
           >
             <h3 className='text-2xl font-bold'>Learn &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
+            <p className='mt-4 text-xl'>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
@@ -51,9 +44,7 @@ const Home: NextPage = () => {
             className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
           >
             <h3 className='text-2xl font-bold'>Examples &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
+            <p className='mt-4 text-xl'>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
@@ -61,9 +52,7 @@ const Home: NextPage = () => {
             className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
           >
             <h3 className='text-2xl font-bold'>Deploy &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p className='mt-4 text-xl'>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -75,8 +64,7 @@ const Home: NextPage = () => {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Powered by{' '}
-          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+          Powered by <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
         </a>
       </footer>
     </div>
